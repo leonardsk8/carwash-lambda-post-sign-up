7eb34ab06f0e71c95ae38b46a49dd391a4af9645
leonardo.martinez <leonardo.martinez@globant.com>
2022-08-26 22:18:45 -0500

    feature: delete presignup function

--- src/main/java/com/carwash/lambda/JDBCConnect.java
+++ src/main/java/com/carwash/lambda/JDBCConnect.java
@@ -19,55 +19,6 @@ public class JDBCConnect {
         this.context = context;
     }
 
-
-    public long saveUser(UserDTO userDTO) {
-        LambdaLogger logger = context.getLogger();
-        logger.log("Invoked JDBCSample.getCurrentTime");
-        long idGenerated= 0L;
-        String sql = "INSERT INTO carwash.user (firts_name, last_name, birth_date," +
-                " address, gender, location, email, phone, city_id,photo, enable, email_confirmed, phone_confirmed, completed, role_id,wash_id)" +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
-        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD); PreparedStatement stmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)) {
-            stmt.setNull(1, Types.VARCHAR);
-            stmt.setNull(2, Types.VARCHAR);
-            stmt.setNull(3, Types.DATE);
-            stmt.setNull(4, Types.VARCHAR);
-            stmt.setNull(5, Types.VARCHAR);
-            stmt.setNull(6, Types.VARCHAR);
-            stmt.setString(7, userDTO.getEmail());
-            stmt.setNull(8, Types.VARCHAR);
-            stmt.setNull(9, Types.INTEGER);
-            stmt.setNull(10, Types.VARCHAR);
-            stmt.setBoolean(11, true);
-            stmt.setInt(12, 0);
-            stmt.setBoolean(13, false);
-            stmt.setBoolean(14, false);
-            stmt.setInt(15, 1);
-            stmt.setNull(16, Types.INTEGER);
-
-
-            int affectedRows = stmt.executeUpdate();
-            logger.log(SUCCESSFULLY_EXECUTED_QUERY + affectedRows);
-
-            if (affectedRows == 0) {
-                throw new SQLException("Creating user failed, no rows affected.");
-            }
-
-            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    idGenerated = generatedKeys.getLong(1);
-                }
-                else {
-                    throw new SQLException("Creating user failed, no ID obtained.");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.log(CAUGHT_EXCEPTION + e.getMessage());
-        }
-        return idGenerated;
-    }
-
     public String updateUser(UserDTO userDTO) {
         LambdaLogger logger = context.getLogger();
         logger.log("Invoked JDBCSample.getCurrentTime");
